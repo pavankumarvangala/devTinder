a973735c9ecc8f98874e485da64b607f7c5ca2ef
pavankumarvangala <45650416+pavankumarvangala@users.noreply.github.com>
2025-06-15 22:33:58 +0530

    testing routing concepts

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userID", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Pawan", lastName: "Vangala" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("Data saved successfully to DB.");
-});
+// app.post("/user", (req, res) => {
+//   res.send("Data saved successfully to DB.");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("User Deleted Successfully from DB.");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("User Deleted Successfully from DB.");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("from Test page");
-});
+// app.use("/test", (req, res) => {
+//   res.send("from Test page");
+// });
 
 app.listen(7777, () => {
   console.log("Server is runnig on Port 7777..");
